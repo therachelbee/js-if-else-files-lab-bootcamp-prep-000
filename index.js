4ea993e22a3a903abd3c40b0872ba2c16936d889
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-01 23:01:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 if (name === "Susan") {
     console.log("The name is correct.")
@@ -11,4 +11,3 @@ if (name === "Susan") {
 // Don't worry about this
 module.exports = { name, height
 }
-
